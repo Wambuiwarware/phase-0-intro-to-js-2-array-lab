6564a2cba14ab2635e4295af1aef26519a92c1cc
Wambui_Maina <you@example.com>
2022-09-29 09:42:55 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
 
 // Write your solution here!
+
+// Write your solution here!
 var cats = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendCat(name){
  cats.push(name)
  return cats;
 }
-
 function appendCat(name){
-    var newArray = []
-    var cats = cats.caats(newArray);
-    cats.push(name)
-    return cats;
+  var newArray = []
+  var cats = cats.caats(newArray);
+  cats.push(name)
+  return cats;
 }
 
 function destructivelyAppendCat(name) {
-   cats.push(name);
-   return cats;
+ cats.push(name);
+ return cats;
 }
 
 function destructivelyPrependCat(name){
-  cats.unshift(name);
-  return cats;
+cats.unshift(name);
+return cats;
 }
 
 function destructivelyRemoveLastCat(){
-  cats.pop();
-  return cats;
+cats.pop();
+return cats;
 }
 
 function destructivelyRemoveFirstCat(){
-  cats.shift();
-  return cats;
+cats.shift();
+return cats;
 }
 
 function appendCat(name){
-  var name_1= [...cats,name];
-  return name_1;
+var name_1= [...cats,name];
+return name_1;
 }
 
 function prependCat(name){
-  var name_2=[name, ...cats];
-  return name_2
+var name_2=[name, ...cats];
+return name_2
 }
 
 function removeLastCat(){
-  var MiniFinalArrayOfPets= cats.slice(0,cats.length-1);
-  return MiniFinalArrayOfPets;
+var MiniFinalArrayOfPets= cats.slice(0,cats.length-1);
+return MiniFinalArrayOfPets;
 }
 
 function removeFirstCat(){
-  var NewArrayOfCats= cats.slice(1)
-  return NewArrayOfCats
+var NewArrayOfCats= cats.slice(1)
+return NewArrayOfCats
 }
 
-// Write your solution here!
+
+
